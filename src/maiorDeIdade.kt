ca9972eb9da0f9ca55658e6940ad1e4642723205
Vitor Eleoterio <vitor.eleoterio@ifood.com.br>
2021-04-23 09:50:26 -0300

    algoritmo Maior Idade

---
+++ src/maiorDeIdade.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+    fun maiorDeIdade(idade: Int){
+        if (idade >= 18){
+            println("Maior de idade")
+        } else {
+            println("Não é maior de idade")
+        }
+    }
+
+    maiorDeIdade(10)
+    maiorDeIdade(18)
+    maiorDeIdade(27)
+}
