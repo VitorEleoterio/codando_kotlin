c86d330fe4b26944733f60ff4573b1d57e50e776
Vitor Eleoterio <vitor.eleoterio@ifood.com.br>
2021-04-23 10:19:12 -0300

    caculaBonus

---
+++ src/calculaBonus.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    fun calculaBonus(cargo: String, salario: Float): Float {
+        var bonus: Float = salario
+        if (cargo == "Coordenador") {
+            bonus = salario * 0.2f
+        } else if (cargo == "Gerente Junior") {
+            bonus = salario * 0.5f
+        } else {
+            bonus = salario * 2
+        }
+
+        return bonus
+
+    }
+
+    println(calculaBonus("Coordenador", 1000f))
+    println(calculaBonus("Gerente Junior", 1000f))
+    println(calculaBonus("Diretor", 1000f))
+}
