323fbc0f89675264a997f3fb75c6c62e6614d4be
Vitor Eleoterio <vitor.eleoterio@ifood.com.br>
2021-04-29 14:06:26 -0300

    Construtores primários e secundários

--- src/Pessoa.kt
+++ src/Pessoa.kt
@@ -1,13 +1,25 @@
 class Pessoa (var nome: String, val anoNasciomento: Int) {
+    var diaNascimento: Int? = null
+    var mesNascimento: String? = null
+    constructor(nome: String,
+                anoNasciomento: Int,
+                diaNascimento: Int,
+                mesNascimento: String
+    ) : this(nome, anoNasciomento){
+        this.diaNascimento = diaNascimento
+        this.mesNascimento = mesNascimento
+    }
+
     fun saudacao() {
-        var ola = println("Olá, meu nome é nome $nome e meu ano de nascimento é $anoNasciomento")
+        var ola = println("Olá, meu nome é nome $nome e minha data de nascimento é $diaNascimento de $mesNascimento de $anoNasciomento")
     }
 }
 
 fun main() {
-    val pessoa = Pessoa("Vitor", 1990)
-
+    val pessoa = Pessoa("Vitor", 1990, 21, "Maio")
     println(pessoa.nome)
+    println(pessoa.diaNascimento)
+    println(pessoa.mesNascimento)
     println(pessoa.anoNasciomento)
-    println(pessoa.saudacao())
+    pessoa.saudacao()
 }
